b21e323294a8eee13ada6cce133b4322b1526b47
Aashika <khatiwadaaashika12@gmail.com>
2026-05-18 15:05:58 +0545

    learning rest api

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from django.urls import path,include
 
-from .views import home_page
+from companyapi.views import home_page
 from rest_framework import routers
-from .views import CompanyViewSet
+from api.views import CompanyViewSet
 
 
 router= routers.DefaultRouter()
 router.register(r'companies',CompanyViewSet)
 urlpatterns =[
     
-    path("", include("router.urls"))
+    path("", include(router.urls))
 ]
--- companyapi/urls.py
+++ companyapi/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
-from .views import home_page
+from companyapi.views import home_page
 
 urlpatterns =[
     path('admin/', admin.site.urls),
